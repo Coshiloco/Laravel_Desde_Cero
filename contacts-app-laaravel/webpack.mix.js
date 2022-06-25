4b89b5eed035838a4d3e44506c37f3574c825e3c
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-25 19:09:44 +0200

    Imagen

--- contacts-app-laaravel/webpack.mix.js
+++ contacts-app-laaravel/webpack.mix.js
@@ -12,6 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/welcome.js', 'public/js/welcome.js')
     .sass('resources/sass/app.scss', 'public/css')
     .css('resources/css/app.css', 'public/css')
     .copy('resources/img', 'public/img')

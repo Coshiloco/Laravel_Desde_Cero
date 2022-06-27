8b4df1ca7667ad45372744fae63bdb5453adfdcc
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-06-27 17:51:04 +0200

    FOrmulario

--- contacts-app-laaravel/resources/views/change_password.blade.php
+++ contacts-app-laaravel/resources/views/change_password.blade.php
@@ -10,7 +10,6 @@
           <div class="card-body">
             <form method="POST" action="/change_password">
               @csrf
-
               <div class="row mb-3">
                 <label for="name"
                   class="col-md-4 col-form-label text-md-end">New Password</label>
